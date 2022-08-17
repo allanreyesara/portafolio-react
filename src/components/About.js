92e3bac5db3dd7f73692515a5f33e9da68f580b0
Allan <a11anr3y3s@gmail.com>
2022-08-17 00:52:23 -0700

    Add About me info

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,18 @@ import '../index.css';
 
 export function About() {
     return(
-        <h2>Adios!!!</h2>
+        <div className="Aboutme-container">
+            <div className="aboutme-text">
+                <h1>About Allan</h1>
+                <p>Hello! I'm a software engineering student from Costa Rica who is searching for new opportunities to learn and grow. My main motivation in this career is to create new things based on my own ideas or from other people.
+                    The idea to create an app or webpage that can last forever on the internet amazes me everyday.</p>
+
+                <p>I am a person who when given a task does not stop until it is finished or gives its best to deliver the best possible final product. Searching for new techniques and knowledge is of utmost importance to me since it allows me to be up to date with the needs of clients and companies, 
+                    in addition to the fact that acquiring new knowledge amuses and entertains me, so it is a relatively easy process for me.</p>
+
+                <p>I currently do not have any experience, but if given an opportunity, I can assure you I will give back the resources and trust invested in me.</p>
+            </div>
+        </div>
     );
 }
 
